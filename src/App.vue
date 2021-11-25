3ec5fa7be2da2af3428496485c11fd8f7a845fd7
Marst83 <244882490@qq.com>
2021-11-25 22:00:14 +0800

    处理初始化文件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>1111</h1>
+    <!-- 根路由出口 -->
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
 
-@Component({
-  components: {
-    HelloWorld
-  }
+export default Vue.extend({
+  name: 'App'
 })
-export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
